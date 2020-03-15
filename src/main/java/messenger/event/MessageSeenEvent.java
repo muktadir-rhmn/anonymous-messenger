d78799aa41642a9e322fae805663bd735ebef922
muktadirrahman <muktadir.rhmn@gmail.com>
2020-03-15 15:11:44 +0600

    added thread id to the response of MessageSeenEvent

--- src/main/java/messenger/event/MessageSeenEvent.java
+++ src/main/java/messenger/event/MessageSeenEvent.java
@@ -2,7 +2,12 @@ package messenger.event;
 
 import java.util.Map;
 
+class MessageSeenEventResponse {
+    public long lastMessageID;
+    public long threadID;
+}
 public class MessageSeenEvent extends Event{
+
 
     public MessageSeenEvent(String userType, Long userID, Long threadID, long lastMessageID) {
         super(userType, userID, threadID);
@@ -11,16 +16,13 @@ public class MessageSeenEvent extends Event{
 
 }
 
-class MessageSeenEventResponse {
-    public long lastMessageID;
-}
-
 class MessageSeenEventResponseGenerator implements EventResponseGenerator {
 
     @Override
     public EventResponse generateResponseData(EventDescriptor eventDescriptor, Map<String, Object> listenerData) {
         MessageSeenEventResponse response = new MessageSeenEventResponse();
         response.lastMessageID = (long) eventDescriptor.data.get("lastMessageID");
+        response.threadID = eventDescriptor.threadID;
         return new EventResponse(eventDescriptor.type, response, eventDescriptor.createdAt);
     }
 }
